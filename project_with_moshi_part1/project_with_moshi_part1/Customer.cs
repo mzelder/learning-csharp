96aabc5de27e2f0de397b52a684f7fc95250e778
mzelder <maciej.zelder@wp.pl>
2024-08-16 00:26:11 +0200

    classes

---
+++ project_with_moshi_part1/project_with_moshi_part1/Customer.cs
@@ -0,0 +1,23 @@
+﻿namespace project_with_moshi_part1
+{
+    partial class Program
+    {
+        public class Customer
+        {
+            public int Id;
+            public string Name;
+
+            public Customer(int id, string name)
+            {
+                this.Id = id;
+                this.Name = name;
+            }
+        }
+
+        //public Customer(int id, string name)
+        //{
+        //    this.Id = id;
+        //    this.Name = name;
+        //}
+    }
+}
---
+++ project_with_moshi_part1/project_with_moshi_part1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_with_moshi_part1
+{
+    partial class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            var customer = new Customer(25, "Maciek");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
