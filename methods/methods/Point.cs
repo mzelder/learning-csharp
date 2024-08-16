2f3835f0a5ea99b9c6eeafb874e73f0ffc12f9b5
mzelder <maciej.zelder@wp.pl>
2024-08-16 12:46:41 +0200

    Exercise with methods of the classes

---
+++ methods/methods/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace methods
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            this.X = newLocation.X;
+            this.Y = newLocation.Y;
+        }
+    }
+}
---
+++ methods/methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstPoint = new Point(5, 10);
+            var secondPoint = new Point(10, 20);
+
+            Console.WriteLine($"{firstPoint.X}, {firstPoint.Y}");
+
+            firstPoint.Move(50, 100);
+            Console.WriteLine($"{firstPoint.X}, {firstPoint.Y}");
+
+            firstPoint.Move(secondPoint);
+            Console.WriteLine($"{firstPoint.X}, {firstPoint.Y}");
+        }
+    }
+}
