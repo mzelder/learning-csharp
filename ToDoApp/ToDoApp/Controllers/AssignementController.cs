55d8bfa156feedf0f6e3f48f72405678a66e0a88
mzelder <maciej.zelder@wp.pl>
2024-08-21 11:22:15 +0200

    Creating templates

---
+++ ToDoApp/ToDoApp/Controllers/AssignementController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Http;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    public class AssignmentController : ApiController
+    {
+        Assignment[] assignements = new Assignment[]
+        {
+            new Assignment { Id = 1, TaskName = "Cleaning room", IsCompleted = false, DueDate = DateTime.Now, Description = "You supposed to clean the room" },
+            new Assignment { Id = 2, TaskName = "Clean floor", IsCompleted = false, DueDate = DateTime.Now, Description = "You supposed to clean the floor" }
+        };
+
+        public IEnumerable<Assignment> GetAllAssignments()
+        {
+            return assignements;
+        }
+        
+        public IHttpActionResult GetAssignement(int id)
+        {
+            var assigment = assignements.FirstOrDefault(x => x.Id == id);
+            if (assigment == null)
+            {
+                return NotFound();
+            }
+            return Ok(assigment);
+        }
+    }
+}
---
+++ ToDoApp/ToDoApp/Models/Assignement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApp.Models
+{
+    public class Assignment
+    { 
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
